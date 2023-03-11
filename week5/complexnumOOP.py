cdb46623930ed5cea6294408e1c9fa8508025ab5
danieldmaestro <momodufresh@gmail.com>
2023-03-11 15:45:55 +0100

    add dataclass

--- week5/complexnumOOP.py
+++ week5/complexnumOOP.py
@@ -23,15 +23,23 @@ class Complex:
 # print(x)
 
 # NAMED TUPLES. some weird type of class and object behavior
-from collections import namedtuple
+# from collections import namedtuple
 
-Card = namedtuple("Card", ['face', 'suit'])
+# Card = namedtuple("Card", ['face', 'suit'])
 
-card = Card(face='Ace', suit='Spades')
-card = Card._make(['Queen', 'Hearts'])
-card2 = Card._make(['King', 'Clubs'])
-print(card.face)
-print(card.suit)
-print(card2.suit)
-print(card2.face)
+# card = Card(face='Ace', suit='Spades')
+# card = Card._make(['Queen', 'Hearts'])
+# card2 = Card._make(['King', 'Clubs'])
+# print(card.face)
+# print(card.suit)
+# print(card2.suit)
+# print(card2.face)
+
+from dataclasses import dataclass
+from typing import ClassVar,List
+
+@dataclass
+class Card:
+    FACES : ClassVar[List[str]] = ['Ace', '2', '3', '4', '5', '6', '7', '8', '9', 'Jack', 'Queen', 'King']
+    SUITS : ClassVar[List[str]] = ['Hearts', 'Diamonds', 'Clubs', 'Spades']
 
