7dc7182fc98a0aaa011e566e35a7aa8d530d8860
Daniel Momodu <momodufresh@gmail.com>
2023-03-17 11:05:45 +0100

    registration form django

--- learn_users/basic_app/views.py
+++ learn_users/basic_app/views.py
@@ -1,3 +1,42 @@
 from django.shortcuts import render
+from basic_app.forms import UserForm,UserProfileInfoForm
 
 # Create your views here.
+def index(request):
+    return render(request, 'basic_app/index.html')
+
+
+def register(request):
+
+    registered = False
+
+    if request.method == 'POST':
+        user_form = UserForm(data=request.POST)
+        profile_form = UserProfileInfoForm(data=request.POST)
+
+        if user_form.is_valid() and profile_form.is_valid():
+
+            user = user_form.save()
+            user.set_password(user.password) #hashes password
+            user.save() #save hashed password
+
+            profile = profile_form.save(commit=False)
+            profile.user = user #sets one-to-one relationship between profile and user
+
+            if 'profile_pic' in request.FILES: #check of there's a profile pic uploaded
+                profile.profile_pic = request.FILES['profile_pic']
+
+            profile.save()
+
+            registered = True
+        else:
+            print(user_form.errors, profile_form.errors)
+
+    else:
+        user_form = UserForm()
+        profile_form = UserProfileInfoForm(        )
+
+    return render(request, 'basic_app/registration.html', 
+                  {'user_form': user_form,
+                   'profile_form': profile_form,
+                   'registered': registered})
--- learn_users/learn_users/settings.py
+++ learn_users/learn_users/settings.py
@@ -16,6 +16,8 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
+MEDIA_DIR = os.path.join(BASE_DIR, 'media')
 
 
 # Quick-start development settings - unsuitable for production
@@ -92,7 +94,6 @@ PASSWORD_HASHERS = [
     'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
     'django.contrib.auth.hashers.BCryptPasswordHasher',
     'django.contrib.auth.hashers.PBKDF2PasswordHasher',
-    'django.contrib.auth.hashers.PBKDF2SHA256PasswordHasher',
     'django.contrib.auth.hashers.ScryptPasswordHasher',
     'django.contrib.auth.hashers.SHA1PasswordHasher',
     'django.contrib.auth.hashers.MD5PasswordHasher',
@@ -103,7 +104,6 @@ PASSWORD_HASHERS = [
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-        'OPTIONS' : {'min-length': 9}
     },
     {
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
@@ -133,6 +133,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [STATIC_DIR]
+
+# MEDIA
+MEDIA_ROOT = MEDIA_DIR
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
