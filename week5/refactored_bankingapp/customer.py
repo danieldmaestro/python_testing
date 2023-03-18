a3afc8e7e9f28fab92c12f0ea84da01d510a268a
danieldmaestro <momodufresh@gmail.com>
2023-03-18 20:41:44 +0100

    design customer class

--- week5/refactored_bankingapp/customer.py
+++ week5/refactored_bankingapp/customer.py
@@ -2,7 +2,7 @@ from random import randint
 from wallet import Wallet
 
 class Customer:
-    def __init__(self, f_name, l_name, phone_no, acct_type, pin, email, acct_no=str(randint(1324145265, 5628158894))):
+    def __init__(self, f_name, l_name, phone_no, acct_type, pin, email):
         self.f_name = f_name
         self.l_name = l_name
         self.phone_no = phone_no
@@ -10,7 +10,6 @@ class Customer:
         self.pin = pin
         self.email = email
         self.is_loggedin = False
-        self.acct_no = acct_no
         self.full_name = self.f_name + " " + self.l_name
         self._wallets = {
             'NGN': None,
@@ -19,15 +18,69 @@ class Customer:
         }
 
     @property
-    def wallet(self, currency):
-        return self._wallets[currency] 
+    def wallet(self):
+        for w in self._wallets.values():
+            if w:
+                print(w)
     
-    @wallet.setter
-    def wallet(self, currency, new_wallet):
+    def add_wallet(self, currency, new_wallet):
         self._wallets[currency] = new_wallet
 
     def create_wallet(self, currency):
         n_wallet = Wallet(currency)
-        self.wallet(currency, n_wallet)
+        self.add_wallet(currency, n_wallet)
+        print(f"You have successfully created your {currency} wallet.")
+
+    def get_wallet(self, currency):
+        return self._wallets[currency]
+
+    def show_all_wallets(self):
+        gross_wallet_balance = 0
+        gross_inflow = 0
+        gross_outflow = 0
+        # print(self._wallets.items())
+
+        for curr, wal in self._wallets.items():
+            if wal:
+                if curr == 'USD':
+                    gross_wallet_balance += wal.current_balance * 735
+                    gross_inflow += wal.total_inflow * 735
+                    gross_outflow += wal.total_outflow * 735
+                elif curr == 'GBP':
+                    gross_wallet_balance += wal.current_balance * 910
+                    gross_inflow += wal.total_inflow * 910
+                    gross_outflow += wal.total_outflow * 910
+                else:
+                    gross_wallet_balance += wal.current_balance
+                    gross_inflow += wal.total_inflow
+                    gross_outflow += wal.total_outflow
+
+        print(f"Dear {self.full_name}, here's your wallet overview:\n")
+        print('=========================================================')
+        for wal in self._wallets.values():
+            if wal:
+                wal.display_wallet()
+                print("=========================================================")
+        print(f"TOTAL RECEIVED: ₦{gross_inflow}")
+        print(f"TOTAL SENT: ₦{gross_outflow}")
+        print(F"TOTAL USER BALANCE: ₦{gross_wallet_balance}")
+
+
+# acct_no=str(randint(1324145265, 5628158894))
+danny = Customer('Daniel', 'Momodu', '09032115544', "Savings", 1234, "danny@gmail.com")
+danny.create_wallet('NGN')
+danny.create_wallet('USD')
+dannyNGN = danny.get_wallet('NGN')
+dannyUSD = danny.get_wallet('USD')
+dannyNGN.deposit(5000)
+dannyUSD.deposit(200)
+dannyNGN.withdrawal(900)
+dannyUSD.withdrawal(23)
+danny.show_all_wallets()
+# print(danny.wallet)
+# print(danny.wallet('USD'))
+
+
+
 
 
--- week5/refactored_bankingapp/wallet.py
+++ week5/refactored_bankingapp/wallet.py
@@ -1,13 +1,13 @@
 import locale
 
-locale.setlocale(locale.LC_ALL, "en-NG")
+# locale.setlocale(locale.LC_ALL, "en-NG")
 
 class Wallet:
     def __init__(self, currency):
         self._balance = 0
         self._currency = currency
         self._outflow = 0
-        self._inflow = 0
+        self._inflow = 0            
 
     @property
     def current_balance(self):
@@ -19,6 +19,20 @@ class Wallet:
             self._balance = value
         else:
             raise ValueError("Value must be above 0")
+        
+    @property
+    def currency(self):
+        return self._currency
+    
+    @property
+    def c_sign(self):
+        if self.currency == "NGN":
+            return '₦'
+        elif self.currency == 'USD':
+            return '$'
+        elif self.currency == 'GBP':
+            return '£'
+    
     @property
     def total_outflow(self):
         return self._outflow
@@ -36,17 +50,17 @@ class Wallet:
         self._inflow = value
 
     def view_current_balance(self):
-        view = locale.currency(self.current_balance, grouping=True)
-        print(f"Your {self._currency} balance is {view}")
+        # view = locale.currency(self.current_balance, grouping=True)
+        print(f"Your {self.currency} balance is {self.c_sign}{self.current_balance:,.2f}")
     
     def deposit(self, amount):
         self.current_balance += amount
         self.total_inflow += amount
-        view_amt = locale.currency(amount, grouping=True)
-        view_bal = locale.currency(self.current_balance, grouping=True)
-        print(f"{view_amt} has been deposited into your account")
+        # view_amt = locale.currency(amount, grouping=True)
+        # view_bal = locale.currency(self.current_balance, grouping=True)
+        print(f"{self.c_sign}{amount:,.2f} has been deposited into your account")
         print('==========================================')
-        print(f"Your current balance is {view_bal}")
+        print(f"Your current balance is {self.c_sign}{self.current_balance:,.2f}")
 
     def inflow(self, amount):
         self.current_balance += amount
@@ -56,11 +70,11 @@ class Wallet:
         if self.current_balance >= amount:
             self.current_balance -= amount
             self.total_outflow = amount
-            view_amt = locale.currency(amount, grouping=True)
-            view_bal = locale.currency(self.current_balance, grouping=True)
-            print(f"You have successfully withdrawn {view_amt}")
+            # view_amt = locale.currency(amount, grouping=True)
+            # view_bal = locale.currency(self.current_balance, grouping=True)
+            print(f"You have successfully withdrawn {self.c_sign}{amount:,.2f}")
             print('==========================================')
-            print(f"Your current balance is {view_bal}")
+            print(f"Your current balance is {self.c_sign}{self.current_balance:,.2f}")
         else:
             print("Withdrawal amount greater than wallet balance.")
     
@@ -68,22 +82,27 @@ class Wallet:
         if self.current_balance >= amount:
             self.current_balance -= amount
             self.total_outflow -= amount
-            recipient.wallet(self._currency).inflow(amount) 
-            view_amt = locale.currency(amount, grouping=True)
-            view_bal = locale.currency(self.current_balance, grouping=True)
-            print(f"You have successfully transferred {view_amt}")
+            recipient.wallet(self.currency).inflow(amount) 
+            # view_amt = locale.currency(amount, grouping=True)
+            # view_bal = locale.currency(self.current_balance, grouping=True)
+            print(f"You have successfully transferred {self.c_sign}{amount:,.2f}")
             print('==========================================')
-            print(f"Your current balance is {view_bal}")
+            print(f"Your current balance is {self.c_sign}{self.current_balance:,.2f}")
         else:
-            pass
+            print("Transfer amount must be less than wallet balance")
+
+    def display_wallet(self):
+        print(f"{self.currency} WALLET:\nWALLET BALANCE: {self.c_sign}{self.current_balance:,.2f}\nTOTAL RECEIVED: {self.c_sign}{self.total_inflow:,.2f}\nTOTAL SENT: {self.c_sign}{self.total_outflow:,.2f}\n")
+
+    def __str__(self):
+        return (f"{self.currency} WALLET: Balance -> {self.c_sign}{self.current_balance:,.2f}")
 
 
 
+# wallet1 = Wallet('NGN')
 
-wallet1 = Wallet('NGN')
-
-wallet1.deposit(5000)
-wallet1.withdrawal(400)
-print(wallet1._inflow)
-print(wallet1._outflow)
+# wallet1.deposit(5000)
+# wallet1.withdrawal(400)
+# print(wallet1._inflow)
+# print(wallet1._outflow)
 
